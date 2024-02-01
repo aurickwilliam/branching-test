209a061e25ac529cbd0051b37e6b5e5685b4374a
aurickwilliam <aurickwilliam21@gmail.com>
2024-02-01 15:48:11 +0800

    Added a Label

--- GUI.py
+++ GUI.py
@@ -11,6 +11,8 @@ class App(ctk.CTk):
         ctk.set_appearance_mode('Dark')
         ctk.set_default_color_theme('green')
 
+        self.label = ctk.CTkLabel(self, text= 'Label')
+        self.label.pack(expand= True)
         self.button = ctk.CTkButton(self, text= 'Button')
         self.button.pack(expand= True)
 
