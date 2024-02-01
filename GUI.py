fa45696a46f1f459f46f20a511b20a5913a24bf3
aurickwilliam <aurickwilliam21@gmail.com>
2024-02-01 15:09:03 +0800

    Added a button

--- GUI.py
+++ GUI.py
@@ -7,7 +7,12 @@ class App(ctk.CTk):
 
         self.title('Git')
         self.geometry('600x400')
+        self.minsize(600, 400)
+        ctk.set_appearance_mode('Dark')
+        ctk.set_default_color_theme('green')
 
+        self.button = ctk.CTkButton(self, text= 'Button')
+        self.button.pack(expand= True)
 
         self.mainloop()
 
